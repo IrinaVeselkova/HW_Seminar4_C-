86da64bb5556458ac50f77fca4916905bb07ca15
Veselkova Irina <veselkovaiv@yandex.ru>
2023-10-18 16:43:33 +0300

    Task 25

---
+++ ex1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+int ReadInt(string msg)// объявляем функцию msg строковую
+{
+Console.WriteLine(msg);//пишет на экран параметр msg
+string a = Console.ReadLine();
+int numb;
+numb = int.Parse(a);//преобразуем строку в число
+return numb;// возврат из функции
+}
+int numb1 = ReadInt("Введите первое число:");
+int numb2 = ReadInt("Введите второе число:");
+
+System.Console.WriteLine($"{numb1}^{numb2} = {Math.Pow(numb1,numb2)}");
