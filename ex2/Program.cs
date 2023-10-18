b594fd88fcf306f398102c60ed3272f5aa7c365b
Veselkova Irina <veselkovaiv@yandex.ru>
2023-10-18 17:12:33 +0300

    Task 27

---
+++ ex2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и
+выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+int ReadInt(string msg)// объявляем функцию msg строковую
+{
+    Console.WriteLine(msg);//пишет на экран параметр msg
+    string a = Console.ReadLine();
+    int numb;
+    numb = int.Parse(a);//преобразуем строку в число
+    return numb;// возврат из функции
+}
+int numb = ReadInt("Введите число:");
+int sum = 0;
+for (int i = 0; i <= numb+10; i++)
+{
+    sum = sum + numb % 10;
+    numb = numb / 10;
+}
+System.Console.WriteLine(sum);
